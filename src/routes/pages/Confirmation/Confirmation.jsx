dbe2b8b7e8baa0bf59ce31e932bb4477fecb1786
Nextgent Org <web.nextgent@gmail.com>
2026-03-03 11:11:19 +0600

    update

--- src/routes/pages/Confirmation/Confirmation.jsx
+++ src/routes/pages/Confirmation/Confirmation.jsx
@@ -111,7 +111,7 @@ export default function Confirmation() {
                 userName: `${user?.firstName || ""} ${user?.lastName || ""}`,
                 userEmail: user?.email,
                 propertyItemIds: itemSummary.map((item) => item.id),
-                paymentMethod: "Online",
+                paymentMethod: "Ziina Payment",
                 serviceFee: servicePrice,
                 serviceCharge,
                 cashOnDelivery: 0,
